e0b214638386c9ea5a04c577d55ec3aa320aab8d
Michael Refvem <mkrefvem@gmail.com>
2020-08-11 19:29:49 -0700

    Register pages/method complete. Registering a new user passes them into the DB and redirects them back to the Home Page.

--- Ecommerce-App/Ecommerce-App/Models/Customer.cs
+++ Ecommerce-App/Ecommerce-App/Models/Customer.cs
@@ -10,5 +10,6 @@ namespace Ecommerce_App.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
     }
 }
