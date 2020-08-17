0ccfeb72f3ef4ef1c7a32ec8098d1089ff0a4e5c
Michael Refvem <mkrefvem@gmail.com>
2020-08-17 15:31:56 -0700

    Sprint 1 submission

--- Ecommerce-App/Ecommerce-App/Data/StoreDbContext.cs
+++ Ecommerce-App/Ecommerce-App/Data/StoreDbContext.cs
@@ -24,7 +24,8 @@ namespace Ecommerce_App.Data
                     SKU = "213nvnv33",
                     Name = "Yamaha C7 Grand Piano",
                     Price = 48000.00m,
-                    Description = "Yamaha's finest concert grand delivers the performance and subtleties expected from the world's leading pianists."
+                    Description = "Yamaha's finest concert grand delivers the performance and subtleties expected from the world's leading pianists.",
+                    Image = "https://401ecommerce.blob.core.windows.net/productimages/Yamaha%20C7%20Grand%20Piano.jpg"
 
                 },
                 new Product
@@ -33,7 +34,8 @@ namespace Ecommerce_App.Data
                     SKU = "33mmml23",
                     Name = "Gemeinhardt Performance Flute",
                     Price = 1749.00m,
-                    Description = "Gemeinhardt's level of craftsmanship has provided excellent flutes for generations of budding professionals. Take advantage of their prestine quality and our great offer today!"
+                    Description = "Gemeinhardt's level of craftsmanship has provided excellent flutes for generations of budding professionals. Take advantage of their prestine quality and our great offer today!",
+                    Image = "https://401ecommerce.blob.core.windows.net/productimages/Gemeinhardt%20Performance%20Flute.jpg"
 
                 }
                 , new Product
@@ -42,7 +44,8 @@ namespace Ecommerce_App.Data
                     SKU = "113nvev33",
                     Name = "Fender Stratocaster",
                     Price = 1149.99m,
-                    Description = "Fender has been a leading innovator in electric guitar building for nearly a century, providing favorite models for legendary musicians such as Jimi Hendrix and more. "
+                    Description = "Fender has been a leading innovator in electric guitar building for nearly a century, providing favorite models for legendary musicians such as Jimi Hendrix and more.",
+                    Image = "https://401ecommerce.blob.core.windows.net/productimages/Fender%20Stratocaster.png"
 
                 },
                  new Product
@@ -51,7 +54,8 @@ namespace Ecommerce_App.Data
                      SKU = "200llvnv33",
                      Name = "SM7B Vocal Microphone",
                      Price = 399.00m,
-                     Description = "Whether it's broadcast, podcast or recording, voices need to be handled with care. When purified and polished, every detail has more impact. That's why the SM7B was built, to capture smooth, warm vocals that connect the speaker to the listener."
+                     Description = "Whether it's broadcast, podcast or recording, voices need to be handled with care. When purified and polished, every detail has more impact. That's why the SM7B was built, to capture smooth, warm vocals that connect the speaker to the listener.",
+                     Image = "https://401ecommerce.blob.core.windows.net/productimages/SM7B%20Vocal%20Microphone.jpg"
 
                  },
                  new Product
@@ -60,7 +64,8 @@ namespace Ecommerce_App.Data
                      SKU = "213nvnv44",
                      Name = "Beats by Dr. Dre",
                      Price = 349.99m,
-                     Description = "These premium noise cancelling headphones ensure the highest fidelity sound can be enjoyed while also blocking out the distractions of everyday noises."
+                     Description = "These premium noise cancelling headphones ensure the highest fidelity sound can be enjoyed while also blocking out the distractions of everyday noises.",
+                     Image = "https://401ecommerce.blob.core.windows.net/productimages/Beats%20by%20Dr.%20Dre.jpg"
 
                  },
                   new Product
@@ -69,7 +74,8 @@ namespace Ecommerce_App.Data
                       SKU = "213oonv33",
                       Name = "Mackie CR3-X 3 inch Multimedia Monitors",
                       Price = 99.00m,
-                      Description = "Mackie CR3-X Multimedia Monitors put studio-quality sound right on your desktop at a great value. Mackie have earned their reputation for quality studio monitoring over the years, and CR3-X Multimedia Monitors are just as suitable for multimedia production as they are for gaming and casual listening."
+                      Description = "Mackie CR3-X Multimedia Monitors put studio-quality sound right on your desktop at a great value. Mackie have earned their reputation for quality studio monitoring over the years, and CR3-X Multimedia Monitors are just as suitable for multimedia production as they are for gaming and casual listening.",
+                      Image = "https://401ecommerce.blob.core.windows.net/productimages/Mackie%20CR3-X%203%20inch%20Multimedia%20Monitors.jfif"
 
                   },
                    new Product
@@ -77,8 +83,9 @@ namespace Ecommerce_App.Data
                        Id = 7,
                        SKU = "122pppp222",
                        Name = "Hohner Special 20 Harmonica",
-                       Price = 5.00m,
-                       Description = "The Honer Special has been a favorite of blues musicians for decades, accompanying some of the legendary recoridngs from the early days of jazz masters all the way to present-day alternative rock."
+                       Price = 49.00m,
+                       Description = "The Honer Special has been a favorite of blues musicians for decades, accompanying some of the legendary recoridngs from the early days of jazz masters all the way to present-day alternative rock.",
+                       Image = "https://401ecommerce.blob.core.windows.net/productimages/Hohner%20Special%2020%20Harmonica.jpg"
 
                    },
                     new Product
@@ -87,7 +94,8 @@ namespace Ecommerce_App.Data
                         SKU = "213dddd33",
                         Name = "Lyon and Healy Concert Harp",
                         Price = 17875.00m,
-                        Description = "Lyon and Healy have set the standard for concert level harp production through their focus on excellence and preference among leading professionals in the industry. They can be heard in almost every major symphony orchestra."
+                        Description = "Lyon and Healy have set the standard for concert level harp production through their focus on excellence and preference among leading professionals in the industry. They can be heard in almost every major symphony orchestra.",
+                        Image = "https://401ecommerce.blob.core.windows.net/productimages/Lyon%20and%20Healy%20Concert%20Harp.jfif"
 
                     },
                      new Product
@@ -96,7 +104,8 @@ namespace Ecommerce_App.Data
                          SKU = "199rrnv33",
                          Name = "Taylor 100 Series Guitar",
                          Price = 799.00m,
-                         Description = "Taylor has been recognized over time for their ability to perfectly capture the backwoods/rustic appeal in Country music. The Taylor 100 Series makes the perfect choice for budding singers across many genres."
+                         Description = "Taylor has been recognized over time for their ability to perfectly capture the backwoods/rustic appeal in Country music. The Taylor 100 Series makes the perfect choice for budding singers across many genres.",
+                         Image = "https://401ecommerce.blob.core.windows.net/productimages/Taylor%20100%20Series%20Guitar.jpg"
 
                      },
                       new Product
@@ -105,7 +114,8 @@ namespace Ecommerce_App.Data
                           SKU = "213oonv00",
                           Name = "Yamaha Absolute Hybrid Maple 5-Piece Drum Set",
                           Price = 3999.00m,
-                          Description = "When Yamaha set out designing the Absolute Hybrid Maple, they started with the most important aspect of a musical instrument, the sound."
+                          Description = "When Yamaha set out designing the Absolute Hybrid Maple, they started with the most important aspect of a musical instrument, the sound.",
+                          Image = "https://401ecommerce.blob.core.windows.net/productimages/Yamaha%20Absolute%20Hybrid%20Maple%205-Piece%20Drum%20Set.jpg"
                       }
 
                 );
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Ecommerce-App: Mojo Music
 Authors: Michael Refvem, Yasir Mohamud
+Contributors: Amanda Iverson, Andrew Casper, Kyungrae Kim, Andrew Smith, Paul Rest, Nicco Ryan, Bryant Davis
+
+# Link to the Deployed Site
+https://mojomusic.azurewebsites.net/
 
 # Introduction 
-This Ecommerce web app displays information about products as well as a home page. The theme of the site is "Mojo Music" and displays various musical instruments and studio products for sale. It has the ability to allow a user to view products, view information about a single product. Users can register, login and logout of their accounts. The first time the seeded Administrator is registered, they are assigned the role of Admin and given claims that allows them special access to different areas of the site, include and Admin portal that is used to allow that person the ability to add photos to Azure Blob.
+This Ecommerce web app displays information about products as well as a home page. The theme of the site is "Mojo Music" and displays various musical instruments and studio products for sale. It has the ability to allow a user to view products, view information about a single product. Users can register, login and logout of their accounts.
+The first time the seeded Administrator is registered, they are assigned the role of Admin and given claims that allows them special access to different areas of the site, including an Admin portal that is used to allow that person the ability to add photos to Azure Blob.
+User Claims that are gathered for every user include First Name, Last Name, Username, Email and Password. These claims do not provide special access to any areas of the site unless that user is also an Admin. These user claims are captured within the RoleInitializer.cs class.
 
 # Getting Started
 To get started using this app:
@@ -16,8 +22,21 @@ The app should simply be able to run through Microsoft Visual Studio.
 
 # Change-Log
 
+17 Aug 2020 - SPRINT 1 Complete
+1.4 Sprint 1
+- *README & Documentation: README contains an introduction to the web application. README contains all required questions as well as link to the deployed site. All contributors are referenced and cited within the README.*
+- *MVC Skeleton & Scaffold: Site contains basic MVC Scaffold including a DBContext with basic products seeded into database. Site implements the repository design pattern with appropriate interfaces and services. Dependency injection is properly registered in Startup.cs file.*
+- *Home Page: Introduction to the site with HTML/CSS. Anonymous accessibility. Home page contains login/register links as well as custom greeting for logged-in users.*
+- *Login Page: Login page consists of username and password form with password masked for visibility. Login page redirect to home page after successful login. Login page access is anonymous allowed. Login page utilizes ASP.NET Core Identity.*
+- *Registration Page: Registration page consists of a form that successfully creates a new account while utilizing ASP.NET Core Identity. Custom claims are properly captured within registration. User is automatically logged in and redirects user to home page after registration. Registration is accessible by anonymous users.*
+- *Products Page: Shopping page is accessible by anonymous users as well as fully displays all products existing in the database. Each product is evenly displayed on the page and shopping page has a clean professional appearance.*
+- *Azure Blob: All images are stored in Azure. Admin panel exists where the admin can create new products.*
+- *User Experience: HTML/CSS is present across the site. Site is clean and professional looking. Pages are properly linked and the overall experience and flow of the site is usable. Site does not contain and unhandled exceptions.*
+- *Azure DevOps Process: Azure DevOps tool is properly maintained. User stories assigned to each member are properly filled out, including time estimation, tasks, branches, description, and acceptance tests.*
+- *Industry Standard: Summary Comments are present and completed. Methods and variable names are appropriate. Fundamentals are properly used. No misuse of code or industry standards present.*
+
 16 Aug 2020
-3.0 Sprint 1 - Milestone #2 Complete
+1.3 Sprint 1 - Milestone #2 Complete
 User Story 1
 - *As a User, I would like the images of my products to be stored securely and external to the project*
 - *Added the ability to store product images securely in the cloud, utilizing Microsoft Azure Blob Storage*
@@ -33,7 +52,7 @@ User Story 4
 - *Administrator given special role and claims to allow them access secret areas of the site*
 
 12 Aug 2020
-2.0 Sprint 1 - Milestone #1 Complete
+1.2 Sprint 1 - Milestone #1 Complete
 User Story 1 
 - *Ability for the user to register for an account on the site, so that they can have a personalized experience*
 - *Added identity for the MVC project*
@@ -46,8 +65,9 @@ User Story 3
 - *Set up a database, StoreDbContext. Registered in the Startup.cs file. Implemented repository design pattern.*
 
 11 Aug 2020
-1.2 *Refactored code base so that Products.cs can be read as a generic class. Created new class, Cereal.cs, that Products.cs then inherits from. Refactored services and controllers according to these changes. Bug fixes: navigating between routes after performing CRUD operations no longer breaks the site. Sort method free of bugs.*
+1.1 *Refactored code base so that Products.cs can be read as a generic class. Created new class, Cereal.cs, that Products.cs then inherits from. Refactored services and controllers according to these changes. Bug fixes: navigating between routes after performing CRUD operations no longer breaks the site. Sort method free of bugs.*
 
 10 Aug 2020
-1.1 *Views, models and controllers implemented. Site functionality: ability to list products and all info, see information on a single product and sort alphabetically and reverse-alphabetically. Basic front end implemented using Bootstrap.*
-1.0 *Commit 62d9bfd0: Initial commit. File tree established, added .gitignore, README.md. Data set, cereal.csv. Directories, Controllers, Models, Views*
+1.0
+- *Views, models and controllers implemented. Site functionality: ability to list products and all info, see information on a single product and sort alphabetically and reverse-alphabetically. Basic front end implemented using Bootstrap.*
+- *Commit 62d9bfd0: Initial commit. File tree established, added .gitignore, README.md. Data set, cereal.csv. Directories, Controllers, Models, Views*
